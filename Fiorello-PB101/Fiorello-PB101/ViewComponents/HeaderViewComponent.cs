681f99b20557fd6da86a2292a581d16ef345bcd3
unknown <asgarkhanmb@code.edu.az>
2024-05-22 02:29:21 +0400

    Practice and Task

---
+++ Fiorello-PB101/Fiorello-PB101/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Fiorello_PB101.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Configuration;
+
+namespace Fiorello_PB101.ViewComponents
+{
+    public class HeaderViewComponent:ViewComponent
+    {
+        private readonly ISettingService _settingsService;
+
+        public HeaderViewComponent(ISettingService settingService)
+        {
+            _settingsService = settingService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View(await _settingsService.GetAllAsync()));
+        }
+    }
+}
